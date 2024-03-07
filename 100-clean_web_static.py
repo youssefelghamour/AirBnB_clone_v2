c5ad5605452cdfe7b74cee81aebf4ef37f9e2e3a
youssef <youssefelghamour@gmail.com>
2024-03-07 08:01:31 -0800

    4. Keep it clean: add 100-clean_web_static.py

---
+++ 100-clean_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Fabric script that deletes out-of-date archives """
+from fabric.api import *
+
+env.hosts = ['18.204.9.11', '18.233.66.83']
+env.user = "ubuntu"
+
+
+def do_clean(number=0):
+    """ Deletes out-of-date archives """
+
+    number = max(1, int(number))
+
+    # Delete local archives except the most recent 'number' ones
+    local('ls -1t versions | tail -n +{} | xargs rm -rf'.format(number + 1))
+
+    # Delete remote archives except the most recent 'number' ones
+    run('ls -1t /data/web_static/releases | tail -n +{} | xargs rm -rf'.
+        format(number + 1))
