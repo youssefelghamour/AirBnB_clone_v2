1678f1933d1213aedce51cf006a44aaf5db05732
youssef <youssefelghamour@gmail.com>
2024-03-24 06:45:07 -0700

    12. HBNB is alive!

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb/', strict_slashes=False)
+def hbnb():
+    """ displays a HTML page """
+    states = sorted(storage.all(State).values(), key=lambda x: x.name)
+    amenities = sorted(storage.all(Amenity).values(), key=lambda x: x.name)
+    places = sorted(storage.all(Place).values(), key=lambda x: x.name)
+
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, places=places)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ closes the database session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
